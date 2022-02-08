9c5000a6b8e5bdefba12be51971bd5f305eee532
nhatduynguyen <nhatduy2211@gmail.com>
2022-02-08 17:02:08 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar, Text, useColorScheme} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import styles from './App.styles';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'light';
+  const bodyText = 'This is not really a bird nest.';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text style={styles.highlight}>{bodyText}</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
